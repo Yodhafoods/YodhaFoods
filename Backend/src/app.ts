df51cd25f7cc752ab6a4a06b79b82c19891c4033
abhissh11 <dev.abhishekkr@gmail.com>
2025-12-16 16:26:42 +0530

    feat: Implement address & order APIs

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,7 +8,7 @@ import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
-
+import addressRoutes from "./routes/address.routes.js";
 
 
 import passport from "./config/passport.js";
@@ -76,6 +76,7 @@ app.use("/api/auth", authLimiter);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
+app.use("/api/addresses", addressRoutes);
 
 
 
