f03eb4d9ae0c0232c0273fbc8bfec3df8f27faf2
RAJEEV SUTRAKAR <sutrakarrajeev22@gmail.com>
2025-12-16 18:38:40 +0530

    Revert "add cart and order api"
    This reverts commit f9fde16182828f93182cc28708972a34416c9b3f.

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,8 +8,8 @@ import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
-import cartRoutes from "./routes/cart.routes.js";
-import orderRoutes from "./routes/order.routes.js";
+
+
 
 import passport from "./config/passport.js";
 
@@ -76,8 +76,6 @@ app.use("/api/auth", authLimiter);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
-app.use("/api/cart", cartRoutes);
-app.use("/api/orders", orderRoutes);
 
 
 
